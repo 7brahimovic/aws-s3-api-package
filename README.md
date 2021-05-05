29c13f1e0c7215f2ca2d1a738cf344d7d7c1e078
7brahimovic <takuyakong@gmail.com>
2021-05-05 22:21:57 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 "# aws-api-package" 
+
+A set of API for AWS S3
+
+Function:
+
+-Zip and download a file
+-Zip and download multiple file
+-Zip and download a folder
+
+-Upload
+-Delete
+-create a JSON for bucket(with file structure)
--- app.js
+++ app.js
@@ -49,28 +49,6 @@ var origins = {
 };
 app.use(cors(origins));
 
-// var storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "public/tempstorage");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, Date.now() + "-" + file.originalname);
-//   },
-// });
-
-// var upload = multer({ storage: storage }).array('file')
-
-// app.post("/upload1", function (req, res) {
-//   upload(req, res, function (err) {
-//     if (err instanceof multer.MulterError) {
-//       return res.status(500).json(err);
-//     } else if (err) {
-//       return res.status(500).json(err);
-//     }
-//     return res.status(200).send(req.file);
-//   });
-// });
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
